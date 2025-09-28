43053a7e7da7f108f81ca01c235f051ffc01b7dd
Shorno <fb.shorno@gmail.com>
2025-09-28 22:40:51 +0600

    remove global loader

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import {Loader} from "lucide-react";
-
-export default function Loading(){
-    return (
-        <div className={"flex min-h-screen justify-center items-center"}>
-            <Loader/>
-        </div>
-    )
-}
